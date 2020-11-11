0d3a01fedaab175171eb7387947707c7635a73bb
Thiago Borges <thiago.borges@theos.com.br>
2020-11-11 15:01:31 -0300

    Criando a client-crud.component e fazendo a navegação entre as duas rotas

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ClientCrudComponent } from './views/client-crud/client-crud.component';
+import { HomeComponent } from './views/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent
+  },
+  {
+    path: "client",
+    component: ClientCrudComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatSidenavModule } from  '@angular/material/sidenav';
 import { MatCardModule } from  '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
 import { HomeComponent } from './views/home/home.component';
+import { ClientCrudComponent } from './views/client-crud/client-crud.component';
 
 @NgModule({
   declarations: [
@@ -22,6 +23,7 @@ import { HomeComponent } from './views/home/home.component';
     FooterComponent,
     NavComponent,
     HomeComponent,
+    ClientCrudComponent,
   ],
   imports: [
     BrowserModule,
