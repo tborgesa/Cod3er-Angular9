5ccea15996a2b63eb08829ce40cc89da283f9a92
Thiago Borges <thiago.borges@theos.com.br>
2020-11-24 12:22:22 -0300

    Docker
    - Melhoria na criação do container do backend para persistir os dados
    - Validação para recriação do container do backend

--- mocked_backend/run.ps1
+++ mocked_backend/run.ps1
@@ -7,6 +7,37 @@ param(
     $porta = 8081
 )
 
-$portaDocker = "$($porta):80"
+$portaContainer = "$($porta):80"
 $location = Get-Location
-docker run -d -p $portaDocker --name $nome -v $location/db.json:/data/db.json clue/json-server
+
+try
+{
+    $dockerVersion = docker version --format '{{.Server.Version}}'
+    Write-Output "Versao docker $dockerVersion"
+}
+catch
+{
+     Write-Output "Docker ainda não existe. Verifique seu sistema operacional, e prossiga para https://docs.docker.com/get-docker/."
+}
+
+$container = docker ps -a --format "{{json .}}" | ConvertFrom-Json | Where-Object {$_.Names -eq $nome}
+
+if ($null -ne $container){
+    docker start $container.ID
+    Write-Output "Container iniciado $($container.ID)"
+    return;
+}
+
+$volume = docker volume ls --format "{{json .}}" | ConvertFrom-Json | Where-Object {$_.Name -eq $nome}
+$volumeContainer = "$($nome):/data"
+
+if ($null -eq $volume){
+    $volume = docker volume create $nome
+    $null = docker container create --name vazio -v $volumeContainer alpine
+    $null = docker cp $location/data/. vazio:/data
+    $null = docker rm vazio
+    Write-Output "Volume criado $volume"
+}
+
+$container = docker run -d -p $portaContainer --name $nome -v $volumeContainer --restart always clue/json-server
+Write-Output "Container criado e iniciado $container"
